4cdf9c8efee18afd252314d1d06058f9d0b0c8dd
Breno Tainan <brenotaguiar@gmail.com>
2022-12-14 18:53:47 -0300

    feat: tela de criente finalizada

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,24 +1,24 @@
 require('dotenv').config();
 module.exports = {
   "development": {
-    "username": process.env.MYSQL_USER,
-    "password": process.env.MYSQL_PASSWORD,
-    "database": process.env.MYSQL_DATABASE,
-    "host": process.env.MYSQL_HOST,
-    "dialect": process.env.MYSQL_DIALECT
+    "username": process.env.MYSQL_USER || process.env.MYSQLUSER,
+    "password": process.env.MYSQL_PASSWORD || process.env.MYSQLPASSWORD,
+    "database": process.env.MYSQL_DATABASE || process.env.MYSQLDATABASE,
+    "host": process.env.MYSQL_HOST || process.env.MYSQLHOST,
+    "dialect": process.env.MYSQL_DIALECT || 'mysq'
   },
   "test": {
-    "username": process.env.MYSQL_USER,
-    "password": process.env.MYSQL_PASSWORD,
-    "database": process.env.MYSQL_DATABASE,
-    "host": process.env.MYSQL_HOST,
-    "dialect": process.env.MYSQL_DIALECT
+    "username": process.env.MYSQL_USER || process.env.MYSQLUSER,
+    "password": process.env.MYSQL_PASSWORD || process.env.MYSQLPASSWORD,
+    "database": process.env.MYSQL_DATABASE || process.env.MYSQLDATABASE,
+    "host": process.env.MYSQL_HOST || process.env.MYSQLHOST,
+    "dialect": process.env.MYSQL_DIALECT || 'mysq'
   },
   "production": {
-    "username": process.env.MYSQL_USER,
-    "password": process.env.MYSQL_PASSWORD,
-    "database": process.env.MYSQL_DATABASE,
-    "host": process.env.MYSQL_HOST,
-    "dialect": process.env.MYSQL_DIALECT
+    "username": process.env.MYSQL_USER || process.env.MYSQLUSER,
+    "password": process.env.MYSQL_PASSWORD || process.env.MYSQLPASSWORD,
+    "database": process.env.MYSQL_DATABASE || process.env.MYSQLDATABASE,
+    "host": process.env.MYSQL_HOST || process.env.MYSQLHOST,
+    "dialect": process.env.MYSQL_DIALECT || 'mysq'
   }
 }
